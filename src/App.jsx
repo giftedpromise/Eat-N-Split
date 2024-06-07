10453489c21feeddbf07417ef756f650e1dae79b
giftedpromise <giftedpromise1992@gmail.com>
2024-06-07 07:58:42 -1200

    setting up

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,79 @@ import React from "react";
 
 import "./App.css";
 
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+function FriendList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend friend={friend} key={friend.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt="friend.name" />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <button className="button">Select</button>
+    </li>
+  );
+}
+
+function FormAddFriend() {
+  return (
+    <form className="">
+      <label htmlFor="">Friend name👩🏼‍🤝‍👩🏻</label>
+      <input type="text" />
+
+      <label>🌄 Image URL</label>
+      <input type="text" />
+    </form>
+  );
+}
+
 function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+      </div>
+    </div>
   );
 }
 
